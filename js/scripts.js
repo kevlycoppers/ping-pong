527fe443f95800b5a4b213e60c1b4902743bc3ed
Levi Hopkins <kevlycoppers@gmail.com>
2016-06-10 09:52:51 -0700

    remove bad code and rework code to run better as well as add three new rules

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,49 @@ var start = 0
 var numberArray = [];
 
 
-var countUp = function(munber){
-  for (var i = 0; i <= number; i++) {
-    number.push(i)
+var count = function(number){
+  for (var i = 1; i <= number; i++) {
+    numberArray.push(i)
+    console.log(number)
   }
 }
 
+var ping = function(number) {
+  numberArray.forEach(function(number) {
+    if (number % 3 === 0) {
+      numberArray[number-1] = "Ping";
+    }
+  });
+}
+
+var pong = function(number) {
+  numberArray.forEach(function(number){
+    if (number % 5 === 0) {
+      numberArray[number-1] = "Pong"
+    }
+  });
+}
+
+var piPo = function(number) {
+  numberArray.forEach(function(number){
+    if (number % 15 === 0) {
+      numberArray[number-1] = "Ping Pong"
+    }
+  });
+}
 //FRONTEND
 $(function() {
   $("form#converter").submit(function(event) {
     event.preventDefault();
 
-    var userNumber = parseInt($("#users-number").val());
-    countUp(userNumber);
+    var inputNumber = parseInt($("#number").val());
+
+    numberArray = [];
+
+    count(inputNumber);
     ping(numberArray);
     pong(numberArray);
+    piPo(numberArray);
+    console.log(numberArray)
   });
 });
